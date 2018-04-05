ed6aa1ce59499a37ab2344ee6145e2658d2cf368
marsovic <marko.arsovic@edu.ece.fr>
2018-04-05 16:56:40 +0200

    Premier code ne marchant pas pour la lecture de fichiers

---
+++ graph_piscine_v0/include/Espece.h
@@ -0,0 +1,16 @@
+#ifndef ESPECE_H
+#define ESPECE_H
+
+
+class Espece
+{
+    public:
+        Espece();
+        virtual ~Espece();
+
+    protected:
+
+    private:
+};
+
+#endif // ESPECE_H
---
+++ graph_piscine_v0/src/Espece.cpp
@@ -0,0 +1,11 @@
+#include "Espece.h"
+
+Espece::Espece()
+{
+    //ctor
+}
+
+Espece::~Espece()
+{
+    //dtor
+}
